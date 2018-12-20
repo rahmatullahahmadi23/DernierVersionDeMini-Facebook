2b3d582474248e016532034d5707ef288f1e3a1b
rahmat ahmadi23 <rahmat>
2018-12-20 08:44:19 +0100

    Jeudi

--- inscription.php
+++ inscription.php
@@ -81,7 +81,7 @@ $per = array();
 
     $fiche->insertPersonne($Nom,$Prenom,$PURL,$Date_naissance,$Statut);
     $personne_id = $connexion->lastInsertId();
-
+    Status :
 
         foreach($per as $key =>$value){
             $fiche->relationPersonne($personne_id, $key, $value);
@@ -110,6 +110,7 @@ $per = array();
 <!DOCTYPE html>
 <html>
 <head>
+
     <link href="https://fonts.googleapis.com/css?family=Aref+Ruqaa" rel="stylesheet">
     <link rel="stylesheet" href="inscription.css">
     <meta charset="utf-8" />
@@ -122,84 +123,104 @@ $per = array();
 </head>
 
 <body>
-    <button id="myButton">Retour</button>
+
+    <a id="Retour" href="Recherche.php" class="buttonRetour">Retour</a>
+ 
     <h1>INSCRIPTION</h1>
     <div class="inscription">
         <form method="get" id="inscription" action="inscription.php">
-            <label class="text">Nom de Famille :</label><br>
-            <input type="text" name="Fname" id="NomDeFamille" class="meme" placeholder="Inserez ici votre nom"><br>
+            <label class="nom">Nom de Famille :</label>
+            <label class="prenom">Prénom</label><br>
+
+            <input type="text" name="Fname" id="NomDeFamille" class="input" placeholder="Inserez ici votre nom">
             
-            <label>Prénom</label><br>
-            <input type="text" name="Pnom" id="prenom" class="meme" placeholder="Inserez ici votre prénom"><br>
+            <input type="text" name="Pnom" id="prenom" class="input" placeholder="Inserez ici votre prénom"><br>
             
-            <label for="">Insertion photo</label><br>
-            <input type="text" id="url" name="pUrl" class="meme" placeholder="URL de Photo"><br>
-            
+            <label for="">Insertion photo</label>
             <label>Date de Naissance :</label><br>
-            <input type="date" id="date" name="bDate" class="meme" min="1900-01-01" max="2500-01-01"><br><br>
+
+            <input type="text" id="url" name="pUrl" class="input" placeholder="URL de Photo">
             
-            <label>Status :</label><br>                
+            <input type="date" id="date" name="bDate" class="date" min="1900-01-01" max="2500-01-01"><br><br>
+            
+            <div class="statut">Statut :</div><br>                
+            <div class="couple">  
+                <label for="celib">Célibataire <input type="radio" name="Status" id="celib" value="Celibataire"></label>            
                 
-                <label for="celib">Célibataire <input type="radio" name="Status" id="celib" value="Celibataire"></label><br>              
-                
-                <label for="couple">En couple <input type="radio" name="Status" id="couple" value="En Couple"></label><br>               
+                <label for="couple">En couple <input type="radio" name="Status" id="couple" value="En Couple"></label>              
                 
                 <label for="pasDef">Pas Défini <input type="radio" name="Status" id="pasDef" value="Pas Defini"></label><br><br>
-            
-            <label>Préférences Musicales :</label><br>                
+                </div>
+            <div class="musique">Préférences Musicales :</div><br>  
+            <div class="couple">            
+                <label for="Rock">Rock </label>       
                 <input type="checkbox" name="Rock" id="Rock">
-                <label for="Rock">Rock </label>                
+         
+                <label for="Pop">Pop </label>           
                 <input type="checkbox" name="Pop" id="Pop">
-                <label for="Pop">Pop </label>                
+     
+                <label for="Rap">Rap </label>         
                 <input type="checkbox" name="Rap" id="Rap">
-                <label for="Rap">Rap </label>               
-                <input type="checkbox" name="Country" id="Country">
+      
                 <label for="Country">Country </label>
+                <input type="checkbox" name="Country" id="Country">
 
-                <input type="checkbox" name="Chill-Out" id="Chill-Out">
+
                 <label for="Chill-Out" >Chill-Out</label>
+                <input type="checkbox" name="Chill-Out" id="Chill-Out">
 
-                <input type="checkbox" name="Jazz" id="Jazz">
+
                 <label for="Jazz" >Jazz</label>
+                <input type="checkbox" name="Jazz" id="Jazz">
 
-                <input type="checkbox" name="Autres" id="Autres...">
-                <label for="Autres..." >Autres...</label><br><br>
 
-            <label>Hobbies :</label><br>               
-                <input type="checkbox" name="Camping" id="Camping">
+                <label for="Autres..." >Autres...</label>
+                <input type="checkbox" name="Autres" id="Autres..."><br><br>
+
+            </div>  
+            <div class="hobbies">Hobbies :</div><br>  
+            <div class="couple">            
                 <label for="velo"> Camping </label>
-                <input type="checkbox" name="Tennis" id="Tennis">
-                <label for="Tennis"> Tennis </label>                
-                <input type="checkbox" name="Golf" id="Golf">
-                <label for="Golf"> Golf </label>
-                <input type="checkbox" name="Echecs" id="Echecs">
-                <label for="Echecs"> Echecs </label>
-                <input type="checkbox" name="Velo" id="Velo">
-                <label for="Velo"> Velo </label>
-                <input type="checkbox" name="Lecture" id="Lecture">
-                <label for="Lecture"> Lecture </label>
-                <input type="checkbox" name="Cinema" id="Cinema">
-                <label for="Cinema"> Cinéma </label>
+                <input type="checkbox" name="Camping" id="Camping">
 
-                <input type="checkbox" name="Autres" id="Autres">
-                <label for="Autres"> Autres...</label> <br><br>
-            
-            <label>Je connais...</label><br>
+                <label for="Tennis"> Tennis </label>   
+                <input type="checkbox" name="Tennis" id="Tennis">
+          
+                <label for="Golf"> Golf </label>
+                <input type="checkbox" name="Golf" id="Golf">
+
+                <label for="Echecs"> Echecs </label>
+                <input type="checkbox" name="Echecs" id="Echecs">
+
+                <label for="Velo"> Velo </label>
+                <input type="checkbox" name="Velo" id="Velo">
+
+                <label for="Lecture"> Lecture </label>
+                <input type="checkbox" name="Lecture" id="Lecture">
+
+                <label for="Cinema"> Cinéma </label>
+                <input type="checkbox" name="Cinema" id="Cinema">
+
+
+                <label for="Autres..."> Autres...</label> 
+                <input type="checkbox" name="Autres..." id="Autres..."><br><br>
+
+            </div> 
+            <div class="connais">Je connais...</div><br>
             <div id="ami">
             <?php
 					$personne = $fiche->selectAllPersonne();
 					 foreach($personne as $key){
 
-                            echo '<p><label name="'.$key->Prenom.'">'.$key->Prenom.' '.$key->Nom.'
-                            <select id="select_option" name="'.$key->Prenom.'">
-                            	<option default></option>
-                            	<option>Ami</option>
-                            	<option>Famille</option>
-                            	<option>Collegue</option>
+                            echo '<p><label name="'.$key->Prenom.'">'.$key->Prenom.' '.$key->Nom.'</label>
+                            <select class="select_option" name="'.$key->Prenom.'">
+                            <option default></option>
+                            <option>Ami</option>
+                            <option>Famille</option>
+                            <option>Collegue</option>
 
                             </select>
-
-                            </label></p>';
+                            </p>';
 
                         } 
 
